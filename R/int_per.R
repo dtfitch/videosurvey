d987d89402ad586307459276bc05dd23d1996bae
Dillon Fitch <dtfitch@ucdavis.edu>
2019-07-28 14:29:41 -0700

    increased max_treedepth

--- R/int_per.R
+++ R/int_per.R
@@ -8,7 +8,7 @@ int_per = brm(comfort_rating_ordered ~ . + (1|person_ID) - person_ID - video_nam
              family=cumulative("logit"), iter = 2000,
              chains = nchains,
              cores = nchains,
-             control = list(adapt_delta = 0.9),
+             control = list(adapt_delta = 0.9,max_treedepth=16),
              prior = c(set_prior("normal(0,5)", class = "b"), 
                        set_prior("student_t(3,0,5)", class = "Intercept"),
                        set_prior("student_t(3,0,5)", class = "sd"))

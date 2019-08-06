04e73de2950313f2859196facc555e155327ff67
jcarlen <jane.carlen@gmail.com>
2019-08-05 19:13:43 -0700

    model_loo now adds loo to each model, saves the result as models_with_loo.RDS. Uses 15 cores

--- R/model_loo.R
+++ R/model_loo.R
@@ -2,13 +2,17 @@
 library(brms)
 models <- lapply(list.files(pattern="_per.RDS|_vid.RDS"), readRDS)
 names(models)<-list.files(pattern="_per.RDS|_vid.RDS")
+ncores = 15
+# this (add_loo?) doesn't work in parallel for some reason. 
 
-# this doesn't work in parallel fro some reason. Also, we need
-# to do this using the loo() function if we want to re-estimate
-# the stan model on the problematic observations. WE probably 
 # should write a function that reads a brms model, calculates 
 # loo using the reloo =T argument, then saves the result into 
 # the loo slot in the model object and saves the model again 
-models <- sapply(models,function(x) add_loo(x,cores=15))
 
+models <- lapply(models,function(x) {
+  loo_tmp = loo::loo(x, reloo = T, ncores = ncores)
+  x$loo = loo_tmp
+  return(x)
+})
 
+saveRDS(models, file = "models_with_loo.RDS")
